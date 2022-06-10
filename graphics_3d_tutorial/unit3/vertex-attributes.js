eb32b2925c9b581e9d81c2657f11013e3f5dacb3
Tyler Townsend <townsend.ts@outlook.com>
2022-06-09 20:51:12 -0700

    color triangle

--- graphics_3d_tutorial/unit3/vertex-attributes.js
+++ graphics_3d_tutorial/unit3/vertex-attributes.js
@@ -14,7 +14,14 @@ function fillScene() {
 	geometry = new THREE.Geometry();
 
 	// Student: add a colored triangle here
+	geometry.vertices.push(new THREE.Vector3(100, 0, 0));
+	geometry.vertices.push(new THREE.Vector3(0, 100, 0));
+	geometry.vertices.push(new THREE.Vector3(0, 0, 100));
 
+	geometry.faces.push( new THREE.Face3(0, 1, 2));
+
+	var colors = [ new THREE.Color(0xFF0000), new THREE.Color(0x00FF00), new THREE.Color(0x0000FF)]
+	geometry.faces[0].vertexColors = colors;
 
 	mesh = new THREE.Mesh( geometry, material );
 
