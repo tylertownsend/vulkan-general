4285e0178ace60e57a1b21653f77dac425c76138
Tyler Townsend <townsend.ts@outlook.com>
2022-08-07 11:16:21 -0700

    7 square and live-server

--- graphics_3d_tutorial/unit8/05-textured-square.js
+++ graphics_3d_tutorial/unit8/05-textured-square.js
@@ -4,7 +4,10 @@
 ////////////////////////////////////////////////////////////////////////////////
 /*global THREE, Coordinates, $, document, window*/
 
-var path = "";	// STUDENT: set to "" to run on your computer, "/" for submitting code to Udacity
+/*
+ * Run with liveServer to make work in vscode. It will server the html from the graphics_3d_tutorial root.
+ */
+var path = "./";	// STUDENT: set to "" to run on your computer, "/" for submitting code to Udacity
 
 var camera, scene, renderer;
 var cameraControls;
@@ -14,28 +17,31 @@ function fillScene() {
 	scene = new THREE.Scene();
 
 	var myPolygon = new SquareGeometry();
-	var myTexture = THREE.ImageUtils.loadTexture( path + 'media/img/cs291/textures/ash_uvgrid01.jpg' );
-	var myPolygonMaterial = new THREE.MeshBasicMaterial( { map: myTexture } );
-	var polygonObject = new THREE.Mesh( myPolygon, myPolygonMaterial );
+	var myTexture = THREE.ImageUtils.loadTexture(path + 'media/img/cs291/textures/ash_uvgrid01.jpg');
+	var myPolygonMaterial = new THREE.MeshBasicMaterial({ map: myTexture });
+	var polygonObject = new THREE.Mesh(myPolygon, myPolygonMaterial);
 	scene.add(polygonObject);
 }
 
 function SquareGeometry() {
 	var geo = new THREE.Geometry();
 
-	// student should add code within this method
+	geo.vertices.push(new THREE.Vector3(0.0, 0.0, 0.0));
+	geo.vertices.push(new THREE.Vector3(1.0, 0.0, 0.0));
+	geo.vertices.push(new THREE.Vector3(1.0, 1.0, 0.0));
+	geo.vertices.push(new THREE.Vector3(0.0, 1.0, 0.0));
 
-	// generate vertices
-	geo.vertices.push( new THREE.Vector3( 0.0, 0.0, 0.0 ) );
-	geo.vertices.push( new THREE.Vector3( 1.0, 0.0, 0.0 ) );
-	geo.vertices.push( new THREE.Vector3( 1.0, 1.0, 0.0 ) );
 	var uvs = [];
-	uvs.push( new THREE.Vector2( 0.0, 0.0 ) );
-	uvs.push( new THREE.Vector2( 1.0, 0.0 ) );
-	uvs.push( new THREE.Vector2( 1.0, 1.0 ) );
+	uvs.push(new THREE.Vector2(0.0, 0.0));
+	uvs.push(new THREE.Vector2(1.0, 0.0));
+	uvs.push(new THREE.Vector2(1.0, 1.0));
+	uvs.push(new THREE.Vector2(0.0, 1.0));
+
 	// generate faces
 	geo.faces.push( new THREE.Face3( 0, 1, 2 ) );
 	geo.faceVertexUvs[ 0 ].push( [ uvs[0], uvs[1], uvs[2] ] );
+	geo.faces.push(new THREE.Face3(0, 2, 3));
+	geo.faceVertexUvs[0].push([uvs[0], uvs[2], uvs[3]]);
 
 	// done: return it.
 	return geo;
@@ -43,18 +49,18 @@ function SquareGeometry() {
 
 function drawHelpers() {
 	// Background grid and axes. Grid step size is 1, axes cross at 0, 0
-	Coordinates.drawGrid({size:100,scale:1,orientation:"z",offset:-0.01});
-	Coordinates.drawAxes({axisLength:2.1,axisOrientation:"x",axisRadius:0.004,offset:-0.01});
-	Coordinates.drawAxes({axisLength:2.1,axisOrientation:"y",axisRadius:0.004,offset:-0.01});
+	Coordinates.drawGrid({ size: 100, scale: 1, orientation: "z", offset: -0.01 });
+	Coordinates.drawAxes({ axisLength: 2.1, axisOrientation: "x", axisRadius: 0.004, offset: -0.01 });
+	Coordinates.drawAxes({ axisLength: 2.1, axisOrientation: "y", axisRadius: 0.004, offset: -0.01 });
 }
 
 function addToDOM() {
 	var container = document.getElementById('container');
 	var canvas = container.getElementsByTagName('canvas');
-	if (canvas.length>0) {
+	if (canvas.length > 0) {
 		container.removeChild(canvas[0]);
 	}
-	container.appendChild( renderer.domElement );
+	container.appendChild(renderer.domElement);
 }
 
 function init() {
@@ -66,19 +72,19 @@ function init() {
 	var canvasRatio = canvasWidth / canvasHeight;
 
 	// RENDERER
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
 	renderer.setSize(canvasWidth, canvasHeight);
-	renderer.setClearColorHex( 0xFFFFFF, 1.0 );
+	renderer.setClearColorHex(0xFFFFFF, 1.0);
 
 	// Camera: Y up, X right, Z up
-	camera = new THREE.PerspectiveCamera( 1, canvasRatio, 50, 150 );
-	camera.position.set( 0.5, 0.5, 100 );
+	camera = new THREE.PerspectiveCamera(1, canvasRatio, 50, 150);
+	camera.position.set(0.5, 0.5, 100);
 
 	// CONTROLS
 	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
-	cameraControls.target.set(0.5,0.5,0);
+	cameraControls.target.set(0.5, 0.5, 0);
 
 }
 
@@ -98,7 +104,7 @@ try {
 	drawHelpers();
 	addToDOM();
 	animate();
-} catch(e) {
+} catch (e) {
 	var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
-	$('#container').append(errorReport+e);
+	$('#container').append(errorReport + e);
 }
